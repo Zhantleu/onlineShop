b75099389d7cf8c314932eb80bfe1b7b3db2f311
Aydos <aidos.jantleu@gmail.com>
2019-02-17 13:39:14 +0600

    Merge fron and back

--- src/main/java/kz/aa/shop/onlineShop/config/WebMvcConfiguration.java
+++ src/main/java/kz/aa/shop/onlineShop/config/WebMvcConfiguration.java
@@ -78,6 +78,7 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
         registry.addResourceHandler("/images/**").addResourceLocations("/images/");
         registry.addResourceHandler("/css/**").addResourceLocations("/css/");
         registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+        registry.addResourceHandler("/lib/**").addResourceLocations("/lib/");
     }
 
 }
