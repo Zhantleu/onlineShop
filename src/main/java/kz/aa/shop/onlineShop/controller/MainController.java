4703e355a404d379c99f7d3cbd2ebf595d9f15db
Aydos <aidos.jantleu@gmail.com>
2019-03-15 23:39:53 +0600

    Fix

--- src/main/java/kz/aa/shop/onlineShop/controller/MainController.java
+++ src/main/java/kz/aa/shop/onlineShop/controller/MainController.java
@@ -1,22 +1,17 @@
 package kz.aa.shop.onlineShop.controller;
 
-import kz.aa.shop.onlineShop.model.User;
 import kz.aa.shop.onlineShop.model.item.Cap;
 import kz.aa.shop.onlineShop.service.impl.CapServiceImpl;
-import kz.aa.shop.onlineShop.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,30 +21,25 @@ import java.util.stream.IntStream;
 public class MainController {
 
     @Autowired
-    private UserServiceImpl userService;
-    @Autowired
     private CapServiceImpl capService;
 
-    @RequestMapping(value = "/home/{page}", method = RequestMethod.GET)
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
     public String home(Model model,
-                       @PathVariable("page") int page) {
+                       @RequestParam(value = "page", defaultValue = "1") int page) {
 
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
         PageRequest pageable = PageRequest.of(page - 1, 2);
-        Page<Cap> asd = capService.findAll(pageable);
-        model.addAttribute("products", asd);
+        Page<Cap> pageCapList = capService.findAll(pageable);
+        model.addAttribute("products", pageCapList);
 
-        int totalPages = asd.getTotalPages();
+        int totalPages = pageCapList.getTotalPages();
         if(totalPages > 0) {
             List<Integer> pageNumbers = IntStream.rangeClosed(1,totalPages).boxed().collect(Collectors.toList());
             model.addAttribute("pageNumbers", pageNumbers);
         }
 
         if (auth != null) {
-            User user = userService.findUserByEmail(auth.getName());
-            model.addAttribute("userName", "Welcome " + user.getName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
-            model.addAttribute("adminMessage", "Content Available Only for all");
             return "view/index";
         }
 
