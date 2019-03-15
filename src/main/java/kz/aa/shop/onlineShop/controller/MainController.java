fa6b9bd3b86066535ab80870838cb900dd7672ae
Aydos <aidos.jantleu@gmail.com>
2019-03-15 21:26:13 +0600

    Pagination&Iteration

--- src/main/java/kz/aa/shop/onlineShop/controller/MainController.java
+++ src/main/java/kz/aa/shop/onlineShop/controller/MainController.java
@@ -5,6 +5,8 @@ import kz.aa.shop.onlineShop.model.item.Cap;
 import kz.aa.shop.onlineShop.service.impl.CapServiceImpl;
 import kz.aa.shop.onlineShop.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -12,10 +14,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class MainController {
@@ -25,13 +30,21 @@ public class MainController {
     @Autowired
     private CapServiceImpl capService;
 
-    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    @RequestMapping(value = "/home/{page}", method = RequestMethod.GET)
     public String home(Model model,
-                       @PageableDefault(sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable) {
+                       @PathVariable("page") int page) {
+
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        List<Cap> asd = capService.findAll();
+
+        PageRequest pageable = PageRequest.of(page - 1, 2);
+        Page<Cap> asd = capService.findAll(pageable);
         model.addAttribute("products", asd);
 
+        int totalPages = asd.getTotalPages();
+        if(totalPages > 0) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1,totalPages).boxed().collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
 
         if (auth != null) {
             User user = userService.findUserByEmail(auth.getName());
