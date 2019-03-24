505c8da938d8b074d48fff97e51caf73c1d86b7b
Azamat <s.ecc.216@gmail.com>
2019-03-24 15:56:39 +0600

    added fucking slider

--- src/main/webapp/js/javascript.js
+++ src/main/webapp/js/javascript.js
@@ -1,7 +1,10 @@
-$('.sl').slick({
-	autoplay: true,
-	autoplaySpeed: 1750,
-	infinite: true,
-	slidesToShow: 3,
-	slidesToScroll: 3
+
+$(document).ready(function(){
+    $('.sl').slick({
+        autoplay: true,
+        autoplaySpeed: 1750,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
 });
---
+++ src/test/java/kz/aa/shop/onlineShop/OrderService.java
@@ -0,0 +1,11 @@
+//package kz.aa.shop.onlineShop;
+//
+//import org.junit.runner.RunWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.junit.MockitoJUnitRunner;
+//
+//@RunWith(MockitoJUnitRunner.class)
+//class OrderService {
+//    @InjectMocks
+//    private
+//}
