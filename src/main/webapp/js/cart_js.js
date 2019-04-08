a3d7ba546a931f560fc2aa6d2b21c25d7cc2aaff
Azamat <s.ecc.216@gmail.com>
2019-04-08 23:59:21 +0600

    calulator and fixed all design without refactoring

--- src/main/webapp/js/cart_js.js
+++ src/main/webapp/js/cart_js.js
@@ -1,3 +1,26 @@
+$(document).ready(function() {
+   for (var i = 0;i<10;i++){
+       var priceTovar = parseFloat(document.getElementById("tovarPrice"+i).innerHTML);
+       var sumTovar = parseInt(document.getElementById("sumOfTovar"+i).value);
+       var subTotal = document.getElementById("subTotal"+i);
+       var sumTest = document.getElementById('sumTest').innerText;
+       if (sumTovar < 0) {
+           subTotal.innerHTML = "Teris san!";
+       } else {
+           subTotal.innerHTML = (priceTovar * sumTovar);
+       }
+   }
+});
+
+$( window ).on( "load", function() {
+    var cikl = 0;
+    for (var b = 0;b<99;b++){
+        var a = parseInt(document.getElementById("subTotal"+b).innerHTML);
+        cikl = cikl + a;
+        document.getElementById('sumTest').innerHTML = cikl;
+        console.log("AA")
+    }
+});
 function countMoney(id_item_price, id_sum_of_item, id_sub_total) {
 
     var priceTovar = parseFloat(document.getElementById(id_item_price).innerHTML);
@@ -9,10 +32,15 @@ function countMoney(id_item_price, id_sum_of_item, id_sub_total) {
         subTotal.innerHTML = "Teris san!";
     } else {
         subTotal.innerHTML = (priceTovar * sumTovar);
-        var asdsad;
-        asdsad = sumTest + subTotal;
-        document.getElementById('sumTest').innerHTML = asdsad;
     }
+
+    var cikl = 0;
+    for (var i = 0;i<99;i++){
+        var a = parseInt(document.getElementById("subTotal"+i).innerHTML);
+        cikl = cikl + a;
+        document.getElementById('sumTest').innerHTML = cikl;
+    }
+
 }
 
 function deleteElem(elem){
