ed9d225dfef4359a3c7c56e8190b3b026bf8814e
Azamat <s.ecc.216@gmail.com>
2019-05-28 10:17:52 +0600

    type category

--- src/main/java/kz/aa/shop/onlineShop/model/property/enumeration/TypeCategory.java
+++ src/main/java/kz/aa/shop/onlineShop/model/property/enumeration/TypeCategory.java
@@ -2,5 +2,20 @@ package kz.aa.shop.onlineShop.model.property.enumeration;
 
 public enum TypeCategory {
     CAP,
-    DOMBRA
+    DOMBRA,
+    ALQA,
+    AYAQKIYM,
+    BILEZIK,
+    DABYL,
+    KOBIZ,
+    KOMZOL,
+    KOYLEK,
+    SAQINA,
+    SAZSYRNAI,
+    SHAPAN,
+    SHASPAY,
+    SHOLPY,
+    SYRGA,
+    TYAQTAS,
+    ZHETYGEN
 }
